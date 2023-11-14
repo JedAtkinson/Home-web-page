a533f37a730f35ef683466de2767faaf0201ed4a
jat200 <jedaatkinson@gmail.com>
2023-11-15 12:42:32 +1300

    Create new react app

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <div>
+          <Routes>
+              <Route index element={<Home />} />
+          </Routes>
+        </div>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
